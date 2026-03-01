d8a6bae89028728aff6c263aad70f7c6cf70e6c3
Meghna Das <md8226@srmist.edu.in>
2026-03-01 17:59:37 +0530

    UC2:Implemented 7-line OOPS banner format

---
+++ Level1/OOPSBannerApp2.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp UC2 OOPS Banner Display Application
+ *
+ * This class extends UC1 to display "OOPS" in a large banner format
+ * using spaces and asterisks across a 7-line grid.
+ *
+ * @author Developer
+ * @version 2.0
+ */
+public class OOPSBannerApp2 {
+    public static void main(String[] args) {
+        // Line-by-line manual string concatenation to form the banner
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
+    }
+}
